c22da1a9fe786615fbedb8174bac9be00f6fe71e
Yapnounkie <edmundopierre@yahoo.com>
2020-12-02 05:33:10 -0800

    add resource student

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_123955) do
+ActiveRecord::Schema.define(version: 2020_12_02_133105) do
 
   create_table "courses", force: :cascade do |t|
     t.string "course_name"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2020_12_02_123955) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["users_id"], name: "index_courses_on_users_id"
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "student_name"
+    t.integer "studentID"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
